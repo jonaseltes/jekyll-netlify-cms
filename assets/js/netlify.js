b1c375bf958b1c7e70db612635d91ab193e72258
Jonas Eltes <jonas@eltes.se>
2019-05-31 20:59:57 +0200

    Update netlify.js

--- assets/js/netlify.js
+++ assets/js/netlify.js
@@ -1,11 +1,9 @@
-<script>
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
-</script>
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
