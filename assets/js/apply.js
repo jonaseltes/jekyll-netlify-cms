9d1692697e82d0b9b159f0347b75a1d8902bb655
Jonas Eltes <jonas@eltes.se>
2019-06-02 01:43:28 +0200

    Form updates

--- assets/js/apply.js
+++ assets/js/apply.js
@@ -1,21 +1,30 @@
 ---
 ---
+
 $(function() {
   // The Javascript
   var id = {{site.data.application-form.form-id | jsonify}};
+
+
   console.log("id: " ,id);
   var form = document.getElementById(id);
   console.log("form: " ,form);
   form.onsubmit = function() {
     var formData = new FormData(form);
-
+    var object = {};
+    formData.forEach(function(value, key){
+        object[key] = value;
+    });
+    console.log("object: " ,object)
     // formData.append('file', file);
-
     var xhr = new XMLHttpRequest();
-    // Add any event handlers here...
     xhr.open('POST', form.getAttribute('action'), true);
     xhr.send(formData);
+    form.reset();
+    $('#application-form-wrapper').fadeOut(500, function(){
+      $('#application-form-wrapper').html('<p class="text-xl">Thank you!</p>').fadeIn(500);
 
+    });
     return false; // To avoid actual submission of the form
   }
 })
