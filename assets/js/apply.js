05447e1885ff6cf5ba4c0c05252aa0898d581c3d
Jonas Eltes <jonas@eltes.se>
2019-06-03 12:42:36 +0200

    updated form for GAS

--- assets/js/apply.js
+++ assets/js/apply.js
@@ -30,31 +30,47 @@ $(function() {
   console.log("id: " ,id);
   var form = document.getElementById(id);
   console.log("form: " ,form);
+  var url = "https://script.google.com/macros/s/AKfycbwXvhNxU0PEUwSqfuyiWHFQYEO_gvjNIZELalBjkHtIYYa_zFuG/exec"
+  var urlDev = "https://script.google.com/a/eltes.se/macros/s/AKfycby9LtcPXDd_OxgAhL-gREUYmCiDuIvs3OUNrgzDeS7P/dev"
+  console.log("url: " ,url);
+  console.log("urlDev: " ,urlDev);
 
-
-  form.onsubmit = function() {
-
-
+  form.onsubmit = function(e) {
+    e.preventDefault();
     var formData = new FormData(form);
     var object = {};
     formData.forEach(function(value, key){
         if (typeof value === 'file' || value instanceof File ) {
           console.log("Found image: " ,value);
           formData.set(key, src);
-          // value = src;
+          value = src;
         }
         object[key] = value;
         console.log("value: " ,value);
     });
     console.log("object: " ,object)
-    // formData.append('file', file);
     var serializedData = $("form").serialize();
     console.log("serializedData:" ,serializedData);
+    var testData = {name:"John Doe", role: "ceo"}
+
+
+    // $.post(url, JSON.stringify(testData)).then(res => {
+    //   console.log("res" ,res);
+    // });
+    //
     $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbzV--xTooSkBLufMs4AnrCTdwZxVNtycTE4JNtaCze2UijXAg8/exec",
-        type: "POST",
+        url: url,
+        type: "post",
         // crossDomain: true,
-        data: "serializedData"
+        data: JSON.stringify(object),
+        // contentType: "application/json",
+        success: function (msg)
+                { console.log("succes: " ,msg) },
+        error: function (err)
+        { console.log(err.responseText)}
+    }).done(function (data, textStatus, xhr) {
+        console.log(xhr.getResponseHeader('Link'));
+        console.log("xhr: " ,xhr);
     });
     // var xhr = new XMLHttpRequest();
     // xhr.open('POST', form.getAttribute('action'), true);
@@ -64,6 +80,8 @@ $(function() {
     //   $('#application-form-wrapper').html('<p class="text-xl">'+confirmation+'</p>').fadeIn(500);
     //
     // });
-    return false; // To avoid actual submission of the form
+    // return false; // To avoid actual submission of the form
   }
+
+
 })
