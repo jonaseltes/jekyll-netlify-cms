2d11083108403b2607f643f74c0e335d39d85749
Jonas Eltes <jonas@eltes.se>
2019-07-01 21:06:24 +0200

    Added fade to menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,9 +11,26 @@ function init() {
 
 }
 
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+
+	if ($(window).scrollTop() > 100){
+ 		$('.header-wrapper').removeClass('bg-none');
+  } else {
+    if (!$('.header-wrapper').hasClass("bg-none")) {
+      $('.header-wrapper').addClass('bg-none');
+    }
+ 	};
+});
+
 $( document ).ready(function() {
   console.log("main.js ready!");
   $('.page-content-container').fadeIn();
+  $('.navbar-toggler').click(function(){
+    if ($(window).scrollTop() < 100){
+      $('.header-wrapper').toggleClass('bg-none');
+    }
+  })
 });
 
 init();
