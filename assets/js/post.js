b1d55bffe0b36e8efcc0691625cb1590797865be
Jonas Eltes <jonas@eltes.se>
2019-12-20 13:24:16 +0100

    Blog 1.0

---
+++ assets/js/post.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  console.log("Post ready!");
+  // $(".post-content").find("img").each(function(){
+  //   $(this).unwrap();
+  // });
+  //
+  // $(".post-content").children().each(function(){
+  //   if ($(this).is("img")) {
+  //     $(this).wrap("<div class='row'></div>");
+  //     var t = $(this);
+  //     var p = $(this).parent();
+  //     console.log("this:" ,t);
+  //     console.log("parent:" ,p);
+  //     while ($(this).parent().next().is("img")) {
+  //       var el = $(this).parent().next().detach();
+  //       // $(el).wrap("<div class='col'></div>");
+  //       console.log("el: " ,el);
+  //       $(p).append(el);
+  //     }
+  //   }
+  // });
+
+});
